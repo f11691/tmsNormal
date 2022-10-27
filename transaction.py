68023a0dbf41c84a6296327ea270d0fc33513f23
Christina Stodt <f.ghovanloo@gmail.com>
2022-10-27 12:40:11 +0200

    add accept function

--- transaction.py
+++ transaction.py
@@ -4,16 +4,33 @@ import numpy as np
 
 # function for set randomly how many request and transaction send by each node per epoch
 def req(know_nodes):
-     reqn = dict()
+     req_dict = dict()
 
      for node in know_nodes:
          request = np.random.uniform(0, 10)
          request = round(request, 1)
-         reqn[node]= request
+         req_dict[node]= request
 
-     return reqn
+     return req_dict
 
 
 # function to calculate how many of request of each node must accept by network based on their trust level
-def acp(reqn, tvalue, malicious_ids):
-    acpn = dict()
+def acp(req_dict, node_trust_value_dict, malicious_ids):
+    accp_dict = dict()
+
+    for node in node_trust_value_dict:
+        if node_trust_value_dict[node] >= 0.5:
+            accp_dict[node] = 100
+        elif 0.5 > node_trust_value_dict[node] >= 0:
+            accp_dict[node] = 80
+        elif 0 > node_trust_value_dict[node] >= -0.1:
+            accp_dict[node] = 50
+        elif -0.1 > node_trust_value_dict[node] >= -0.5:
+            accp_dict[node] = 30
+        elif -0.5 > node_trust_value_dict[node] >= -1:
+            accp_dict[node] = 0
+
+    return accp_dict
+
+
+# or calculate how many of them accepted from request numbers?!?!
