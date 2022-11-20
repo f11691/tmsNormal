5fe91c99766ad9fa569a0b2c3ce32b39db09a7fe
Jan Stodt <jan.stodt@hs-furtwangen.de>
2022-11-21 00:17:43 +0100

    Revert "BROKEN: Trying to fix things"
    This reverts commit c3adc73387a1877bb271d556a459512b8908199d.

--- tms.py
+++ tms.py
@@ -1,44 +1,43 @@
-def trust_value(node, last_X_epochs, trustscore, df_calculation):  # remove the items from trust score lists that their key id are malicious
-    # print("this is our last_X_epochs")
-    print("OUR NODE IS: %s" % node)
-    print("THIS IS INSIDE THE CALC...")
-    print(df_calculation)
-    m_rate = df_calculation["M_Rate"].item()
-    last_epoch_tv = last_X_epochs.loc[last_X_epochs["Node_ID"] == node]["Trust_Value"].item()
+def trust_value(know_nodes, malicious_ids, tms_last_X_required_epochs, last_X_epochs,
+                trustscore):  # remove the items from trust score lists that their key id are malicious
 
-    trustvalue = (trustscore[node] + last_epoch_tv * 2 + m_rate) / 4
+    trustvaluedict = dict()
 
-    """"
-    if node in malicious_ids:
-        # 10 thing
-        trustvalue = (0 + trustscore[node] + last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
-            "Trust_Value"].mean(axis=0) * 6) / 10
-        trustvalue = round(trustvalue, 1)
-        trustvaluedict[node] = trustvalue
-    else:
-        # 7 thing
-        trustvalue = (trustscore[node] + last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
-            "Trust_Value"].mean(axis=0) * 6) / 7
-        trustvalue = round(trustvalue, 1)
-        trustvaluedict[node] = trustvalue
+    if tms_last_X_required_epochs != len(last_X_epochs):
+        for node in know_nodes:
+            if node in malicious_ids:
+                # 10 thing
+                trustvalue = (0 + trustscore[node] + last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
+                    "Trust_Value"].mean(axis=0) * 6) / 10
+                trustvalue = round(trustvalue, 1)
+                trustvaluedict[node] = trustvalue
+            else:
+                # 7 thing
+                trustvalue = (trustscore[node] + last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
+                    "Trust_Value"].mean(axis=0) * 6) / 7
+                trustvalue = round(trustvalue, 1)
+                trustvaluedict[node] = trustvalue
+
+        for k, _ in trustvaluedict.items():
+            if trustvaluedict[k] == 0.0 or trustvaluedict[k] == -0.0:
+                trustvaluedict[k] = abs(0)
+
+        return trustvaluedict
     """
-
-    return abs(trustvalue)
-"""
-else:
-    # Potentially dead code
-    # Known dublicate: maybe remove if logic is not different
-    for node in know_nodes:
-        if node == malicious_ids:
-            # 10 thing
-            trustvalue = (-3 + trustscore[node] + last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
-                "Trust_Value"].mean(axis=0) * 6) / 10
-            trustvalue = round(trustvalue, 1)
-            trustvaluedict[node] = trustvalue
-        else:
-            # 7 thing
-            trustvalue = (trustscore[node] + last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
-                "Trust_Value"].mean(axis=0) * 6) / 7
-            trustvalue = round(trustvalue, 1)
-            trustvaluedict[node] = trustvalue
-"""
+    else:
+        # Potentially dead code
+        # Known dublicate: maybe remove if logic is not different
+        for node in know_nodes:
+            if node == malicious_ids:
+                # 10 thing
+                trustvalue = (-3 + trustscore[node] + last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
+                    "Trust_Value"].mean(axis=0) * 6) / 10
+                trustvalue = round(trustvalue, 1)
+                trustvaluedict[node] = trustvalue
+            else:
+                # 7 thing
+                trustvalue = (trustscore[node] + last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
+                    "Trust_Value"].mean(axis=0) * 6) / 7
+                trustvalue = round(trustvalue, 1)
+                trustvaluedict[node] = trustvalue
+    """
