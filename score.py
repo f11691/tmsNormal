fix TMS formula- no there is no bouncing

score.py
@@ -21,6 +21,6 @@ class Score:  # calculate trust score of each subnet
         total_0_axis = np.average(convert, axis=0)  # array of summation of columns
         trustscore = dict()
         for index, neighbour in enumerate(neighbours):
-            trustscore[int(neighbour)] = round(total_0_axis[index], 1)
+            trustscore[int(neighbour)] = round(total_0_axis[index], 4)
 
         return trustscore

tms.py
@@ -1,5 +1,5 @@
-def trust_value(know_nodes, malicious_ids, tms_last_X_required_epochs, last_X_epochs,
-                trustscore):  # remove the items from trust score lists that their key id are malicious
+def trust_value(know_nodes, malicious_ids, tms_last_X_required_epochs, last_X_epochs, trustscore,
+                m_rate):  # remove the items from trust score lists that their key id are malicious
 
     trustvaluedict = dict()
 
@@ -7,20 +7,21 @@ def trust_value(know_nodes, malicious_ids, tms_last_X_required_epochs, last_X_ep
         for node in know_nodes:
             if node in malicious_ids:
                 # 10 thing
-                trustvalue = (0 + trustscore[node] + last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
-                    "Trust_Value"].mean(axis=0) * 6) / 10
-                trustvalue = round(trustvalue, 1)
+                trustvalue = (trustscore[node] + (
+                            last_X_epochs.loc[last_X_epochs["Node_ID"] == node]["Trust_Value"].mean(axis=0) * 2) +
+                              m_rate[node]) / 4
+                trustvalue = round(trustvalue, 3)
                 trustvaluedict[node] = trustvalue
             else:
                 # 7 thing
-                trustvalue = (trustscore[node] + last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
-                    "Trust_Value"].mean(axis=0) * 6) / 7
-                trustvalue = round(trustvalue, 1)
+                trustvalue = (trustscore[node] + (last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
+                                                      "Trust_Value"].mean(axis=0) * 2) + m_rate[node]) / 4
+                trustvalue = round(trustvalue, 3)
                 trustvaluedict[node] = trustvalue
 
         for k, _ in trustvaluedict.items():
             if trustvaluedict[k] == 0.0 or trustvaluedict[k] == -0.0:
-                trustvaluedict[k] = abs(0)
+                trustvaluedict[k] = round(abs(0), 4)
 
         return trustvaluedict
     """