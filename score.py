Fix old average bug

score.py
@@ -18,9 +18,8 @@ class Score:  # calculate trust score of each subnet
         neighbours = convert[0]
         # Delete first row
         convert = np.delete(convert, 0, 0)
-        total_0_axis = np.average(convert, axis=0)  # array of summation of columns
+        total_0_axis = np.nanmean(convert, axis=0)  # array of summation of columns
         trustscore = dict()
         for index, neighbour in enumerate(neighbours):
             trustscore[int(neighbour)] = round(total_0_axis[index], 5)
-
         return trustscore

tms.py
@@ -10,21 +10,29 @@ def trust_value(know_nodes, malicious_ids, tms_last_X_required_epochs, last_X_ep
                 trustvaluedict[node] = trustvalue
             elif node in malicious_ids:
                 # 10 thing
-                trustvalue = ((trustscore[node]*1) + (
-                            last_X_epochs.loc[last_X_epochs["Node_ID"] == node]["Trust_Value"].mean(axis=0)*2) +
-                              m_rate[node]*2)/ 5
+                trustvalue = ((trustscore[node] * 1) + (
+                        last_X_epochs.loc[last_X_epochs["Node_ID"] == node]["Trust_Value"].mean(axis=0) * 2) +
+                              (m_rate[node] * 4)) / 7
                 trustvalue = round(trustvalue, 5)
                 if trustvalue < 0:
                     trustvalue = 0
+                if trustvalue > 1:
+                    trustvalue = 1
                 trustvaluedict[node] = trustvalue
             else:
                 # 7 thing
-                trustvalue = ((trustscore[node]*1) + (last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
-                                                      "Trust_Value"].mean(axis=0)*2) + m_rate[node]*2) / 5
+                trustvalue = ((trustscore[node] * 1) + (last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
+                                                            "Trust_Value"].mean(axis=0) * 2) + (m_rate[node] * 4)) / 7
                 trustvalue = round(trustvalue, 5)
                 if trustvalue < 0:
                     trustvalue = 0
+                if trustvalue > 1:
+                    trustvalue = 1
                 trustvaluedict[node] = trustvalue
+
+            print(
+                "%s: %s, %s, %s " % (node, (trustscore[node] * 1), (last_X_epochs.loc[last_X_epochs["Node_ID"] == node][
+                                                                        "Trust_Value"].mean(axis=0)), (m_rate[node])))
 
         for k, _ in trustvaluedict.items():
             if trustvaluedict[k] == 0.0 or trustvaluedict[k] == -0.0:

voting.py
@@ -30,7 +30,7 @@ class Voter:  # nodes as voters
         vote = np.zeros((len(self.neighbours) + 1, len(self.neighbours) + 1))
         for i in range(len(self.neighbours) + 1):
             if i == 0:
-                vote[0, i] = 0
+                vote[0, i] = np.nan
             else:
                 vote[0, i] = self.neighbours[i - 1]
                 vote[i, 0] = self.neighbours[i - 1]
@@ -42,24 +42,37 @@ class Voter:  # nodes as voters
         for x in range(1, len(self.neighbours) + 1):
             for y in range(1, len(self.neighbours) + 1):
                 if x == y:
-                    pass
+                    v = np.nan
+                    vote[x, y] = v
                 else:
                     # dont loop
                     # look at avg_dict[y]
                     # for node_avg, node_sig in zip(avg_dict, sig_dict):
+                    """
                     if node_trust_value_dict[y] > (avg_dict[y] + sig_dict[y]):
-                        v = node_trust_value_dict[y] + random.uniform(0.1, 0.2)
+                        v = node_trust_value_dict[y] - random.uniform(0.05, 0.1)
+                        # v = node_trust_value_dict[y]
                         if v > 1:
                             v = 1
                     elif (avg_dict[y] - sig_dict[y]) <= node_trust_value_dict[y] <= (avg_dict[y] + sig_dict[y]):
-                        v = node_trust_value_dict[y] + random.uniform(0, 0.1)
+                        v = node_trust_value_dict[y] + random.uniform(0.1, 0.2)
 
                     elif (avg_dict[y] - sig_dict[y]) > node_trust_value_dict[y]:
+                        v = node_trust_value_dict[y] + random.uniform(0.05, 0.1)
+                        # v = node_trust_value_dict[y]
+                        if v < 0:
+                            v = 0
+                    """
+                    if (node_trust_value_dict[y] - avg_dict[y]) >= 0:
+                        v = node_trust_value_dict[y] + random.uniform(0.1, 0.2)
+                    elif (node_trust_value_dict[y] - avg_dict[y]) < 0:
                         v = node_trust_value_dict[y] - random.uniform(0.05, 0.1)
                         if v < 0:
                             v = 0
                     else:
                         v = 9999999
+                    if v > 1:
+                        v = 1
                     vote[x, y] = v
 
         return vote